59d2149e6b034df9272ef6f681e3ed34bd4cd3aa
Sysprogs <sysprogs@sysprogs.com>
2019-07-02 16:45:48 -0700

    Added a basic application for programming the STM32WB wireless stack

---
+++ STM32/STM32WBUpdater/DeviceConnectionRequestWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using STM32WBUpdater.DeviceEnumeration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace STM32WBUpdater
+{
+    /// <summary>
+    /// Interaction logic for DeviceConnectionRequestWindow.xaml
+    /// </summary>
+    public partial class DeviceConnectionRequestWindow : Window
+    {
+        readonly STM32WBUpdaterConfiguration _Configuration;
+        DispatcherTimer _Timer;
+
+        public DeviceConnectionRequestWindow(STM32WBUpdaterConfiguration config)
+        {
+            InitializeComponent();
+
+            _Configuration = config;
+            _Timer = new DispatcherTimer(TimeSpan.FromMilliseconds(500), DispatcherPriority.Normal, CheckForDevices, Dispatcher.CurrentDispatcher);
+            _Timer.Start();
+        }
+
+        private void CheckForDevices(object sender, EventArgs e)
+        {
+            try
+            {
+                var regex = new Regex(_Configuration.SupportedDeviceIDRegex);
+
+                using (var set = new DeviceInformationSet())
+                {
+                    var devices = set.GetAllDevices().Where(d => regex.IsMatch(d.HardwareID)).ToArray();
+                    if (devices.Length == 1)
+                        Close();
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            _Timer.Stop();
+        }
+
+        private void Help_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Skip_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ STM32/STM32WBUpdater/UploadWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+
+namespace STM32WBUpdater
+{
+    /// <summary>
+    /// Interaction logic for UploadWindow.xaml
+    /// </summary>
+    public partial class UploadWindow : Window
+    {
+        private readonly STM32WBUpdaterConfiguration _Configuration;
+        private string _DataDirectory;
+
+        public class ControllerImpl : INotifyPropertyChanged
+        {
+            public STM32WBUpdaterConfiguration Configuration { get; }
+
+            public STM32WBUpdaterConfiguration.ProgrammableBinary SelectedBinary
+            {
+                get
+                {
+                    return selectedBinary;
+                }
+
+                set
+                {
+                    selectedBinary = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedBinary)));
+                }
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            public ControllerImpl(STM32WBUpdaterConfiguration cfg)
+            {
+                Configuration = cfg;
+            }
+
+            private STM32WBUpdaterConfiguration.ProgrammableBinary selectedBinary;
+        }
+
+        public readonly ControllerImpl Controller;
+
+        public UploadWindow()
+        {
+            InitializeComponent();
+            Loaded += UploadWindow_Loaded;
+
+            _DataDirectory = "data";
+
+            var ser = new XmlSerializer(typeof(STM32WBUpdaterConfiguration));
+            using (var fs = File.OpenRead(System.IO.Path.Combine(_DataDirectory, "STM32WBUpdater.xml")))
+                _Configuration = (STM32WBUpdaterConfiguration)ser.Deserialize(fs);
+
+            DataContext = Controller = new ControllerImpl(_Configuration);
+        }
+
+        private void UploadWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var wnd = new DeviceConnectionRequestWindow(_Configuration) { Owner = this };
+            wnd.ShowDialog();
+        }
+    }
+}
